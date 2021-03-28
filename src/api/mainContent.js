c521fe43ca9dbbe597cf568e2ac5420f0414438c
Naveen <info.naveensachdeva@gmail.com>
2021-03-28 13:41:51 +0530

    Updated Readme + udpated Main.js to handle list of sections

---
+++ src/api/mainContent.js
@@ -0,0 +1,37 @@
+export const getMainContent = () => {
+    return [
+        {
+            id: 1,
+            title: "Interfaces (C#)",
+            text: `An interface contains definitions for a group of related functionalities 
+            that a non-abstract class or a struct must implement. An interface may define static methods, 
+            which must have an implementation. Beginning with C# 8.0, an interface may define a default 
+            implementation for members. An interface may not declare instance data such as fields, 
+            auto-implemented properties, or property-like events.`
+        },
+        {
+            id: 2,
+            title: "Interfaces (C#)",
+            text: `An interface contains definitions for a group of related functionalities 
+            that a non-abstract class or a struct must implement. An interface may define static methods, 
+            which must have an implementation. Beginning with C# 8.0, an interface may define a default 
+            implementation for members. An interface may not declare instance data such as fields, 
+            auto-implemented properties, or property-like events.`
+        }
+    ];
+
+};
+
+
+// return [
+//     {
+//         id: 1,
+//         name: "introduction",
+//         submenu: {
+//             1: "Add React to Website",
+//             2: "Create a new react app",
+//             3: "CDN links",
+//             4: "Release Channels"
+//         }
+//     }
+// ];
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import PageHeader from "./helperComponents/Main/PageHeader";
 import Header from "./helperComponents/Main/Header";
-import SubHeader from "./helperComponents/Main/SubHeader";
+// import SubHeader from "./helperComponents/Main/SubHeader";
 import TextContent from "./helperComponents/Main/TextContent";
+
+import { getMainContent } from "../api/mainContent";
 
 import '../styles/main.css';
 
-function Main(props){
 
-    return (
-        <div className="main">
-            <div id="page-root">
-                <PageHeader>Introduction</PageHeader>
-                <Header>Heading</Header>
-                <TextContent>some text here...</TextContent>
-                <SubHeader>Sub Heading</SubHeader>
-                <TextContent>some text here...</TextContent>
+function Main(props) {
+
+    const [content, setContent] = useState([]);
+
+    useEffect(() => {
+        fetchContent();
+    }, []);
+
+    const fetchContent = () => {
+        setContent(getMainContent());
+        // console.log(content)
+    };
+
+    const renderContent = () => {
+        return content.map(item => {
+            return <React.Fragment key={item.id}>
+                <Header>{item.title}</Header>
+                <TextContent>{item.text}</TextContent>
+            </React.Fragment>
+
+        })
+
+    };
+
+    if (content.length > 0) {
+
+        return (
+            <div className="main">
+                <div id="page-root">
+                    <PageHeader>C#</PageHeader>
+
+                    <Header>{content[0].title}</Header>
+                    <TextContent>{content[0].text}</TextContent>
+
+                    {renderContent()}
+                    {/* <SubHeader>Sub Heading</SubHeader>
+                    <TextContent>some text here...</TextContent> */}
+                </div>
             </div>
-
-        <br />
-        
-        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-        <br /><br /><br /><br /><br /><br />
-        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-        <br /><br /><br /><br /><br /><br />
-        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-        <br /><br /><br /><br /><br /><br />
-        <br />
-        Content End
-        </div>
-    );
+        );
+    } else {
+        return <div>Loading....</div>
+    }
 };
 
 export default Main;
