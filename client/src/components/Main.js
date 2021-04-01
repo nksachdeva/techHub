ef93ee5f417eb9d0bc5d83cea107ffd6f5d06ed7
Naveen <info.naveensachdeva@gmail.com>
2021-04-01 12:14:37 +0530

    added Dockerfile + updated directory structure

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -16,7 +16,7 @@ function Main(props) {
 
     useEffect(() => {
         fetchContent();
-    });
+    },[]);
 
     const fetchContent = async () => {
         const maincontent = await getMainContent();
